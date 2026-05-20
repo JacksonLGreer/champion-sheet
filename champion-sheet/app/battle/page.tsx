25e07357a17847e1a25f102a536e1202b95006d7
JacksonLGreer <jlg40585@uga.edu>
2026-05-19 21:10:27 -0400

    pages

---
+++ champion-sheet/app/battle/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { useEffect, useState } from "react";
+import { fetchPokemonFromAPIName, Pokemon, getAllPokemon } from "../Services/pokemon-service";
+
+
+export default function Battle() {
+
+    return (
+        <div className="w-full h-screen bg-green-900 flex items-center justify-center">
+            <h1 className="text-4xl text-white font-bold">Battle Page</h1>
+        </div>  
+    )
+}
